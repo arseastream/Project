718ab71c1ce730612343e779d1bc2d619a7f23d1
Andrew Seastream <arseastream@gmail.com>
2023-12-31 05:09:53 -0500

    Add logic for calculating ages.

--- App.py
+++ App.py
@@ -84,7 +84,8 @@ primer = """You are a helpful assistant.
             Do not call st.pyplot without an argument, this will be deprecated soon.
             If you are asked to plot, create a line plot without markers, make sure it includes a title and axis names, and show the plot on the streamlit using st.pyplot.
             If you plot, make sure the x-axis labels are rotated if they are long, and use ha="right".
-            If you plot actuals, plot them in blue. If you plot model, plot them in red."""
+            If you plot actuals, plot them in blue. If you plot model, plot them in red.
+            If you need to calculate the difference between two dates in months, do this directly using dt.year and dt.month."""
 
 # Additional primer to be ended at the end of the prompt
 prompt_addition = """"""
